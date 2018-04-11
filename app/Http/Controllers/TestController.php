287e596aa4078801473c456f33542e46a55fd327
lishanlei <2686685661@qq.com>
2018-04-11 08:13:40 +0800

    config functions

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\Request;
 class TestController {
 
-    public function request() {
+    public function request(Request $request) {
         echo "the request is shoudao!";
     }
 }
