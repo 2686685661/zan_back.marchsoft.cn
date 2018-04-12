ed638a3596f7174958f92ed132e7086dfdf3b9f2
star <1060701529@qq.com>
2018-04-12 21:52:31 +0800

    配置

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\login::class,
         ],
 
         'api' => [
@@ -59,5 +60,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'cors' => \App\Http\Middleware\Cors::class,
         'ip' => \App\Http\Middleware\Ip::class,
+        'login'=>\App\Http\Middleware\login::class,
     ];
 }
