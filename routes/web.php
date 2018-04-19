c1f0493c645f5af3b069b9fa4be55dd90059647d
lishanlei <2686685661@qq.com>
2018-04-19 22:42:29 +0800

    新增 支付宝支付接口完成，回调没写

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,17 @@
 //     return view('welcome');
 // });
 
+
+
 Route::group(['middleware'=>['cors']], function() {
-    Route::get('/','IndexController@index');
+    // Route::get('/','IndexController@index');
     include('user.php');
     include('admin.php');
+
+    Route::group(['prefix' => 'alipay'],function() {
+        Route::get('wappay','User\Alipay\AlipayWapController@alipayWapPay');
+        Route::get('return','User\Alipay\AlipayWapController@alipayReturn');
+        Route::get('notify','User\Alipay\AlipayWapController@alipayNotify');
+    });
 });
 
